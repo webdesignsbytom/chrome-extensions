2b1299ed026b1980436b56c305ca64c0868a2b5c
xtombrock <xtombrock1989@gmail.com>
2024-09-10 17:42:57 +0100

    init

--- ai_blocker/src/scripts/main.js
+++ ai_blocker/src/scripts/main.js
@@ -1,3 +1,11 @@
-document.getElementById("myButton").addEventListener("click", () => {
-    alert("Button clicked!");
+console.log('main.js');
+
+document.getElementById('toggleBlocker').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.scripting.executeScript({
+        target: { tabId: tabs[0].id },
+        files: ['src/scripts/content.js']
+      });
+    });
   });
+  
